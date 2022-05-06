349a3365e6bebd9a0d4742a2db8787ae1113f02f
hd4n <hajosd01@gmail.com>
2022-05-06 20:12:16 +0200

    main test

--- graphics/src/blind-virologists/main/Main.java
+++ graphics/src/blind-virologists/main/Main.java
@@ -6,6 +6,9 @@ import effects.*;
 import items.*;
 import map.*;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,8 +16,32 @@ import java.io.PrintStream;
 import java.util.*;
 
 
-public class Main {
+public class Main extends JFrame {
+
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    public Main() {
+        super("alma");
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+    //a paint (vagy paintComponent) overrideolasaval kapod meg a komponens Grapics-et
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        draw(g2d);
+    }
+
+    public void draw(Graphics2D g) {
+        g.drawRect(50, 50, 50, 50);
+    }
+
     public static void main(String[] args) {
-        System.out.println("graphics");
+        Main m = new Main();
+        m.pack();
+        m.setVisible(true);
     }
 }
