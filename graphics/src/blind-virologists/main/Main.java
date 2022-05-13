3df27543fb41d6f238d282c76d6ce85d7db4fa52
Eros Pal <erospali01@gmail.com>
2022-05-13 11:32:31 +0200

    tipusellenorzes kijavitasa,  GameWindow meg nincsen kesz

--- graphics/src/blind-virologists/main/Main.java
+++ graphics/src/blind-virologists/main/Main.java
@@ -1,28 +1,9 @@
 package main;
 
-
-import views.MapGenerator;
-
-import citizens.Citizen;
-import citizens.Virologist;
-import effects.*;
-import items.*;
-import map.*;
-import views.EmptyView;
-import views.FieldView;
 import views.MenuView;
 
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-
-
-public class Main extends JFrame {
+public class Main{
 
     public static void main(String[] args) {
         MenuView game = new MenuView();
