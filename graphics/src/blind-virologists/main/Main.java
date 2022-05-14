6376ffaaad04a1a695e8c8f982f263dd24910c46
Eros Pal <erospali01@gmail.com>
2022-05-14 16:25:50 +0200

    main komment

--- graphics/src/blind-virologists/main/Main.java
+++ graphics/src/blind-virologists/main/Main.java
@@ -1,27 +1,22 @@
 package main;
 
-
-import views.MapGenerator;
-
-import citizens.Citizen;
-import citizens.Virologist;
-import effects.*;
-import items.*;
-import map.*;
-import views.EmptyView;
-import views.FieldView;
 import views.MenuView;
-
-
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
-
+/**
+ * <h1>A vilagtalan virologusok vilaga</h1>
+ * <h2>Laborfeladat</h2>
+ * <h3>Szoftver projekt laboratorium</h3>
+ * <h3>Budapesti Muszaki es Gazdasagtudomanyi Egyetem Villamosmernoki es Informatikai kar</h3>
+ *
+ * @since 2022-05-15
+ * @author 19 - alma
+ * @author Barta Daniel Zsolt
+ * @author Eros Pal
+ * @author Feher Norbert Zsolt
+ * @author Hajos Daniel
+ * @author Kovacs Aron
+ */
 public class Main extends JFrame {
 
     public static void main(String[] args) {
--- graphics/src/blind-virologists/views/MenuView.java
+++ graphics/src/blind-virologists/views/MenuView.java
@@ -1,19 +1,12 @@
 package views;
 
 import citizens.Virologist;
-import effects.Dance;
-import effects.Forget;
-import effects.Immunity;
 import items.*;
-import main.Main;
 import map.City;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Objects;
 
