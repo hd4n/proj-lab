4085c748b3607b3f36429089b473d49d0d49b648
hd4n <hajosd01@gmail.com>
2022-05-15 19:55:56 +0200

    update path

--- graphics/src/blind-virologists/main/Main.java
+++ graphics/src/blind-virologists/main/Main.java
@@ -20,7 +20,7 @@ import java.io.File;
  */
 public class Main {
 
-    public static final String wdPath = new File(System.getProperty("user.dir")).getParent() + "/proj-lab/graphics";
+    public static final String wdPath = new File(System.getProperty("user.dir")).getParent() /*+ "/proj-lab/graphics"*/;
 
     public static void main(String[] args) {
         System.out.println(wdPath);
